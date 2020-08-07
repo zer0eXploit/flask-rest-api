d554872a40dcead04fbf780836fab5361499b5a4
Yan Waipann <yanwaipann@gmail.com>
2020-08-07 14:48:34 +0630

    Remove tables creation codes

--- app.py
+++ app.py
@@ -19,12 +19,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # creates /auth
 
 api.add_resource(Store, "/store/<string:name>")
